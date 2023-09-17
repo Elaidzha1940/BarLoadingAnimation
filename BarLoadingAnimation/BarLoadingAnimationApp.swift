215aeb23ce90a5ee7673eed3d189811ae5111052
Elaidzha1940 <eli080293@gmail.com>
2023-09-17 15:27:31 +0300

    BarLoadingAnimation

---
+++ BarLoadingAnimation/BarLoadingAnimationApp.swift
@@ -0,0 +1,21 @@
+//  /*
+//
+//  Project: BarLoadingAnimation
+//  File: BarLoadingAnimationApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 17.09.2023
+//
+//  Status: #Completed | Decorated
+//
+//  /*
+
+import SwiftUI
+
+@main
+struct BarLoadingAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BarLoadingAnimation/ContentView.swift
@@ -0,0 +1,27 @@
+//  /*
+//
+//  Project: BarLoadingAnimation
+//  FIle: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 17.09.2023
+//
+//  Status: #Completed | Decorated
+//
+//  */
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+        
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
