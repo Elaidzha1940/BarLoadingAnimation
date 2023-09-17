59769105d04454260813fff189fcd67312131b06
Elaidzha1940 <eli080293@gmail.com>
2023-09-17 15:42:12 +0300

    BarLoadingAnimation

--- BarLoadingAnimation/ContentView.swift
+++ BarLoadingAnimation/ContentView.swift
@@ -12,10 +12,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var height0 = 40.0
+    @State var height1 = 40.0
+    @State var height2 = 70.0
+    @State var height3 = 60.0
+    @State var height4 = 85.0
+  
+    
     var body: some View {
         
-        VStack {
+        HStack {
         
+            RoundedRectangle(cornerRadius: 20)
+                .frame(width: 30, height: height0)
+                .foregroundColor(.white)
+                .animation(.easeInOut(duration: 0.25), value: height0)
+            
+            RoundedRectangle(cornerRadius: 20)
+                .frame(width: 30, height: height1)
+                .foregroundColor(.white)
+                .animation(.easeInOut(duration: 0.25), value: height1)
+            
+            RoundedRectangle(cornerRadius: 20)
+                .frame(width: 30, height: height2)
+                .foregroundColor(.white)
+                .animation(.easeInOut(duration: 0.25), value: height2)
+            
+            RoundedRectangle(cornerRadius: 20)
+                .frame(width: 30, height: height3)
+                .foregroundColor(.white)
+                .animation(.easeInOut(duration: 0.25), value: height3)
+            
+            RoundedRectangle(cornerRadius: 20)
+                .frame(width: 30, height: height4)
+                .foregroundColor(.white)
+                .animation(.easeInOut(duration: 0.25), value: height4)
+            
+        }
+        .preferredColorScheme(.dark)
+        .onAppear {
+            
+            Timer.scheduledTimer(withTimeInterval: 0.25, repeats: true) { Timer in
+                
+                height0 = Double.random(in: 20...40)
+                height1 = Double.random(in: 20...60)
+                height2 = Double.random(in: 20...90)
+                height3 = Double.random(in: 20...50)
+                height4 = Double.random(in: 20...30)
+                
+            }
         }
     }
 }
